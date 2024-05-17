ab92d0dc90d998b726b97f35978de24b01b45516
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-05-17 20:58:29 +0530

    Create Solution.cpp

---
+++ 1300 - 1399/1325. Delete leaves with a given value/Solution.cpp
@@ -0,0 +1,9 @@
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+        if root.left is None and root.right is None and root.val == target:
+            return None
+        return root
