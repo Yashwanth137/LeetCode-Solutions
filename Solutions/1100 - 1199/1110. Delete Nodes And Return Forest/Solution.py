3f7247eea17f472dbe1bce8f8e6431de42d80b5f
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-07-18 19:55:01 +0530

    Create Solution.py

---
+++ Solutions/1100 - 1199/1110. Delete Nodes And Return Forest/Solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def delNodes(
+        self, root: Optional[TreeNode], to_delete: List[int]
+    ) -> List[TreeNode]:
+        def dfs(root: Optional[TreeNode]) -> Optional[TreeNode]:
+            if root is None:
+                return None
+            root.left, root.right = dfs(root.left), dfs(root.right)
+            if root.val not in s:
+                return root
+            if root.left:
+                ans.append(root.left)
+            if root.right:
+                ans.append(root.right)
+            return None
+
+        s = set(to_delete)
+        ans = []
+        if dfs(root):
+            ans.append(root)
+        return ans
