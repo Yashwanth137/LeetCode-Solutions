3a91e39700ad51df9423e775d32e6a56e701891a
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-09-30 21:13:41 +0530

    Create Solution.py

---
+++ Solutions/1300 - 1399/1381. Design a Stack With Increment Operation/Solution.py
@@ -0,0 +1,19 @@
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.maxSize = maxSize
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.maxSize:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        if self.stack:
+            return self.stack.pop()
+        return -1
+
+    def increment(self, k: int, val: int) -> None:
+        limit = min(k, len(self.stack))
+        for i in range(limit):
+            self.stack[i] += val
