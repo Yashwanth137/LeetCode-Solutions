618730a31977101dd62984efbbaaf1d71e224600
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-10-26 18:38:29 +0530

    Update Solution.py

--- Solutions/2400-2499/2458. Height of Binary Tree After Subtree Removal Queries/Solution.py
+++ Solutions/2400-2499/2458. Height of Binary Tree After Subtree Removal Queries/Solution.py
@@ -1,11 +1,3 @@
-from typing import List, Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
     def treeQueries(self, root: Optional[TreeNode], queries: List[int]) -> List[int]:
         height = {}
