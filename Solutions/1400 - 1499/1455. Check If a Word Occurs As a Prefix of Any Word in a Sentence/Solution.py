ee18e95884185e0b09fa83590e7d419d328d0906
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-12-02 13:48:10 +0530

    Create Solution.py

---
+++ Solutions/1400 - 1499/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        words = sentence.split()
+
+        for i in range(len(words)):
+            if len(words[i]) >= len(searchWord):
+                if words[i].startswith(searchWord):
+                    return i+1
+                    
+        return -1
