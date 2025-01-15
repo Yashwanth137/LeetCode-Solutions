267482d0dafb36c11d6e1aafa071be412023ac9f
Yash <75321458+Yashwanth137@users.noreply.github.com>
2025-01-15 12:00:29 +0530

    Update Solution.py

--- Solutions/0100-200/169. Majority Element/Solution.py
+++ Solutions/0100-200/169. Majority Element/Solution.py
@@ -1,6 +1,21 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        x=None
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate, count = 0, 0
+
         for i in nums:
-            if nums.count(i)>math.floor(len(nums)/2):
-                return i
+            if count == 0:
+                candidate = i
+                count = 1
+            elif i == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        count = 0
+        for i in nums:
+            if i == candidate:
+                count += 1
+        
+        if count > len(nums)//2:
+            return candidate
+        
