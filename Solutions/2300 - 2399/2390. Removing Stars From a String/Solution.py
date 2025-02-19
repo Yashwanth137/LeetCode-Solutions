68c00845a961529a7a45d46bf76c7d5a2331c8f3
Yash <75321458+Yashwanth137@users.noreply.github.com>
2025-02-19 19:05:32 +0530

    Create Solution.py

---
+++ Solutions/2300 - 2399/2390. Removing Stars From a String/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+        for i in s:
+            if i != '*':
+                stack.append(i)
+            else:
+                stack.pop()
+        
+        return ''.join(stack)
